5e22ffbb091fa6fd65616eec1bb6999a4aad3623
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-04-22 12:14:21 +0530

    Added project setup with Vite added es-lint config file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div>
+      Worldwise
+    </div>
+  )
+}
+
+export default App
+
+// "npm i eslint vite-plugin-eslint eslint-config-react-app --save-dev"
+// "create .eslintrc.json file"
