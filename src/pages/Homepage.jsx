2de3a64fae5aed94051b009ca023246de2595f5b
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-04-22 15:20:11 +0530

    Added CSS module and some starter files

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
     <div>
       <PageNav />
       <h1>WorldWise</h1>
-      <Link to="/pricing">Pricing</Link>
+      <Link to="/app">Go to the App</Link>
     </div>
   );
 }
