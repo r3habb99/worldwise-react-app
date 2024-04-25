0db8db27b73fe405af83c9bbe021d50b5d21682c
Rishabh Prajapati <rishabh.prajapati@brainvire.com>
2024-04-25 15:26:20 +0530

    Added custom Context file and BackButton Component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,8 +3,10 @@ import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import Message from "./Message";
+import { useCities } from "../contexts/citiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
